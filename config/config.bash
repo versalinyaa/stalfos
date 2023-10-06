f7912e4e89046b0270df078ea17cac7c1019716f
Lina Versace <lina@kiwitree.net>
2023-10-06 10:25:15 -0700

    fixes

--- config/config.bash
+++ config/config.bash
@@ -2,6 +2,6 @@
 
 : "${XDG_CACHE_HOME:="$HOME/.cache"}"
 : "${STALFOS_CACHE_DIR:="$XDG_CACHE_HOME/stalfos"}"
-: "${STALFOS_DISK_SIZE:='128GiB'}"
-: "${STALFOS_ROOT_PASSWD:='test0000'}"
-: "${STALFOS_ARCH_MIRROR:='https://mirrors.edge.kernel.org/archlinux'}"
+: "${STALFOS_DISK_SIZE:=128GiB}"
+: "${STALFOS_ROOT_PASSWD:=test0000}"
+: "${STALFOS_ARCH_MIRROR:=https://mirrors.edge.kernel.org/archlinux}"
