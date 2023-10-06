b7f997b98686b8f9c72ba251590b4c336c772ef9
Lina Versace <lina@kiwitree.net>
2023-10-06 11:34:23 -0700

    fixes

--- config/config.bash
+++ config/config.bash
@@ -4,4 +4,12 @@
 : "${STALFOS_CACHE_DIR:="$XDG_CACHE_HOME/stalfos"}"
 : "${STALFOS_DISK_SIZE:=128GiB}"
 : "${STALFOS_ROOT_PASSWD:=test0000}"
+
+# The mirror used to bootstrap the image's chroot.
 : "${STALFOS_ARCH_MIRROR:=https://mirrors.edge.kernel.org/archlinux}"
+
+# bool 0/1
+: "${STALFOS_PACMAN_REFLECTOR_ENABLE:=0}"
+# Comma-separted list of country codes to use when generating the image's pacman
+# mirrorlist. The special code '*' matches all countries.
+: "${STALFOS_PACMAN_REFLECTOR_COUNTRIES:=US}"
